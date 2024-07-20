ee3c84ed197bfe051b3cd1f085f12bf1139d57c3
Your Name <you@example.com>
2024-07-20 13:43:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold">Daniel</h1>
+    </div>
+  );
+}
+
+export default App;
